828e4e2993ab1ac28c8c31906b05315898e9ee9e
songjunlong <songjunlong@air-x.com.cn>
2018-01-18 10:17:18 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,153 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<div class="layout">
+		<Layout>
+			<Header>
+				<Menu mode="horizontal" theme="dark" active-name="1">
+					<div class="layout-logo">树莓派传感器</div>
+					<div class="layout-nav">
+						<MenuItem name="1">
+						<Icon type="ios-navigate"></Icon>
+						Item 1
+						</MenuItem>
+						<MenuItem name="2">
+						<Icon type="ios-keypad"></Icon>
+						Item 2
+						</MenuItem>
+						<MenuItem name="3">
+						<Icon type="ios-analytics"></Icon>
+						Item 3
+						</MenuItem>
+						<MenuItem name="4">
+						<Icon type="ios-paper"></Icon>
+						Item 4
+						</MenuItem>
+					</div>
+				</Menu>
+			</Header>
+			<Layout>
+				<Sider hide-trigger :style="{background: '#fff'}">
+					<Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+						<MenuItem name="1">
+						<Icon type="ios-speedometer"></Icon>
+						传感器概览
+						</MenuItem>
+						<Submenu name="1">
+							<template slot="title">
+								<Icon type="ios-navigate"></Icon>
+								Item 1
+							</template>
+							<MenuItem name="1-1">Option 1</MenuItem>
+							<MenuItem name="1-2">Option 2</MenuItem>
+							<MenuItem name="1-3">Option 3</MenuItem>
+						</Submenu>
+						<Submenu name="2">
+							<template slot="title">
+								<Icon type="ios-keypad"></Icon>
+								Item 2
+							</template>
+							<MenuItem name="2-1">Option 1</MenuItem>
+							<MenuItem name="2-2">Option 2</MenuItem>
+						</Submenu>
+						<Submenu name="3">
+							<template slot="title">
+								<Icon type="ios-analytics"></Icon>
+								Item 3
+							</template>
+							<MenuItem name="3-1">Option 1</MenuItem>
+							<MenuItem name="3-2">Option 2</MenuItem>
+						</Submenu>
+					</Menu>
+				</Sider>
+				<Layout :style="{padding: '0 24px 24px'}">
+					<Breadcrumb :style="{margin: '24px 0'}">
+						<BreadcrumbItem>首页</BreadcrumbItem>
+						<BreadcrumbItem>概览</BreadcrumbItem>
+					</Breadcrumb>
+					<Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+						<router-view/>
+						<h2>	我的书桌的温度</h2>
+						<g2-line :charData="serverData" :id="'c1'" id="c1"></g2-line>
+					</Content>
+				</Layout>
+			</Layout>
+		</Layout>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	import { fetchPostUrlencoded, fetchGet, fetchGetHaveParam } from './utils/requestHttp.js'
+	import { getLocalTime } from './utils/common.js'
+
+	import G2Line from './components/G2Line.vue';
+	export default {
+		components: {
+			G2Line
+		},
+		data() {
+			return {
+				serverData: [],
+			}
+		},
+		created() {
+			this.saveService('10000');
+		},
+		methods: {
+			saveService(id) {
+				let _self = this;
+				let tempArr = [];
+				fetchPostUrlencoded('/www/?m=api&c=api&a=datasteamlist', {
+					id: id,
+				}).then((res) => {
+					if(res.status !== 0) {
+						_self.$Message.error(res.msg);
+					} else {
+						_self.$Message.success(res.message);
+						let ge = parseInt(res.data.data.length / 10);
+						for(var i = 0; i < res.data.data.length; i++) {
+							res.data.data[i]['strftime'] = (res.data.data[i]['datetime']);
+							res.data.data[i]['value'] = res.data.data[i]['data'];
+						}
+						//_self.serverData = res.data.data;
+						for(let i = 0; i < res.data.data.length; i++) {
+							if(i % ge == 0) {
+								tempArr.push(res.data.data[i]);
+							}
+						}
+						tempArr.pop();
+						tempArr.push(res.data.data[res.data.data.length - 1]);
+						_self.serverData = tempArr;
+					}
+				});
+			},
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+	.layout {
+		border: 1px solid #d7dde4;
+		background: #f5f7f9;
+		position: relative;
+		border-radius: 4px;
+		overflow: hidden;
+	}
+	
+	.layout-logo {
+		width: 100px;
+		height: 30px;
+		background: none;
+		float: left;
+		/*
+		position: relative;
+		top: 15px;
+		left: 20px;
+		*/
+		color: #FFFFFF;
+	}
+	
+	.layout-nav {
+		width: 420px;
+		margin: 0 auto;
+		margin-right: 20px;
+	}
 </style>
---
+++ src/components/G2Line.vue
@@ -0,0 +1,105 @@
+<template>
+	<div :id="id"></div>
+</template>
+
+<script>
+	import G2 from 'g2';
+	export default {
+		data() {
+			return {
+				chart: null
+			};
+		},
+		props: {
+			charData: {
+				type: Array,
+				default: function() {
+					return {
+						data: [{
+								'mzkId': 112,
+								'strftime': "2017-01-11",
+								'value': 9275501
+							}, // 测试数据，根据自己需求自己设置数据
+							{
+								'mzkId': 112,
+								'strftime': "2017-01-12",
+								'value': 9281904
+							},
+							{
+								'mzkId': 112,
+								'strftime': "2017-01-13",
+								'value': 9290777
+							},
+							{
+								'mzkId': 112,
+								'strftime': "2017-01-14",
+								'value': 9297913
+							},
+							{
+								'mzkId': 112,
+								'strftime': "2017-01-15",
+								'value': 9306918
+							},
+							{
+								'mzkId': 112,
+								'strftime': "2017-01-16",
+								'value': 9315641
+							}
+						]
+					};
+				}
+			},
+			id: String
+		},
+		mounted: function() {
+			//      this.drawChart();       // 第一步想到的是创建的时候更新图表，但是这个不适用于异步请求接口获取相关数据，所以采用下面的监听的方式
+		},
+		beforeUpdate: function() {
+			//      this.drawChart();
+		},
+		watch: {
+			charData: function(val, oldVal) { // 监听charData，当放生变化时，触发这个回调函数绘制图表
+				console.log('new: %s, old: %s', val, oldVal);
+				this.drawChart(val);
+			}
+		},
+		methods: {
+			drawChart: function(datas) {
+				// 如果图形存在则删除再创建，这个地方感觉稍微有点坑
+				// 具体的G2 api函数说明请看上面提供的官网地址，此处不再逐一说明
+				this.chart && this.chart.destroy();
+				let data = datas;
+				this.chart = new G2.Chart({
+					id: this.id,
+					height: 500,
+					forceFit: true,
+				});
+				this.chart.source(data, {
+					strftime: {
+						alias: '时间',
+						type: 'cat',
+						range: [0, 1]
+					},
+					value: {
+						alias: '温度( C )'
+					}
+				});
+				this.chart.line().position('strftime*value').size(2);
+				this.chart.point().position('strftime*value').color('#757373')
+					.shape('circle')
+					.label('value', {
+						offset: 20,
+						label: {
+							fill: '#000'
+						}
+					});
+				this.chart.animate(false);
+				this.chart.render();
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
